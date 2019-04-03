a2ab60c4630e897b31d94d565cf01c24f7ec8924
Philippe Jean Guillaumie <similar@orange.fr>
2019-04-03 20:43:41 +0200

    Add comment

--- emeritux.sh
+++ emeritux.sh
@@ -578,6 +578,7 @@ remov_preq() {
   if [ -d $DOCDIR/sources/$ICNV ]; then
     echo
     beep_question
+    # Questions: Press Enter to accept the default values.
     read -t 10 -p "Remove libiconv installed from tarball? [Y/n] " answer
     case $answer in
     [yY])
