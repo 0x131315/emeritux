ed41bfee5db3f57050cfc517067008ba406a7c87
Philippe Jean Guillaumie <similar@orange.fr>
2019-04-06 14:48:41 +0200

    Refactor build rebuild* functions

--- emeritux.sh
+++ emeritux.sh
@@ -225,7 +225,8 @@ build_optim() {
     case $I in
     efl)
       meson . build/
-      meson configure -Dharfbuzz=true -Dbindings=luajit,cxx -Dbuildtype=release build/
+      meson configure -Dharfbuzz=true -Dbindings=luajit,cxx -Devas-loaders-disabler= \
+        -Dbuildtype=release build/
       ninja -C build/ || mng_err
       ;;
     enlightenment)
@@ -258,7 +259,8 @@ rebuild_optim() {
     case $I in
     efl)
       sudo chown $USER:$USER build/.ninja*
-      meson configure -Dharfbuzz=true -Dbindings=luajit,cxx -Dbuildtype=release build/
+      meson configure -Dharfbuzz=true -Dbindings=luajit,cxx -Devas-loaders-disabler= \
+        -Dbuildtype=release build/
       ninja -C build/ || mng_err
       ;;
     enlightenment)
@@ -292,8 +294,8 @@ rebuild_wld() {
     case $I in
     efl)
       sudo chown $USER:$USER build/.ninja*
-      meson configure -Dharfbuzz=true -Dbindings=luajit,cxx -Ddrm=true -Dwl=true -Dopengl=es-egl \
-        -Dbuildtype=release build/
+      meson configure -Dharfbuzz=true -Dbindings=luajit,cxx -Devas-loaders-disabler= \
+        -Ddrm=true -Dwl=true -Dopengl=es-egl -Dbuildtype=release build/
       ninja -C build/ || mng_err
       ;;
     enlightenment)
