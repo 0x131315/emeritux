ae09908c64a4f9fd876cba49f4da792fd494ea12
Philippe Jean Guillaumie <similar@orange.fr>
2019-08-23 09:36:29 +0200

    Disable build examples in efl

--- emeritux.sh
+++ emeritux.sh
@@ -238,7 +238,7 @@ build_optim() {
     efl)
       meson . build/
       meson configure -Dnative-arch-optimization=true -Dharfbuzz=true -Dbindings=luajit,cxx -Dbuild-tests=false \
-        -Dbuildtype=release build/
+        -Dbuild-examples=false -Dbuildtype=release build/
       ninja -C build/ || mng_err
       ;;
     enlightenment)
@@ -292,7 +292,7 @@ rebuild_optim() {
     efl)
       sudo chown $USER:$USER build/.ninja*
       meson configure -Dnative-arch-optimization=true -Dharfbuzz=true -Dbindings=luajit,cxx -Dbuild-tests=false \
-        -Dbuildtype=release build/
+        -Dbuild-examples=false -Dbuildtype=release build/
       ninja -C build/ || mng_err
       ;;
     enlightenment)
@@ -341,7 +341,7 @@ rebuild_wld() {
     efl)
       sudo chown $USER:$USER build/.ninja*
       meson configure -Dnative-arch-optimization=true -Dharfbuzz=true -Dbindings=luajit,cxx -Ddrm=true -Dwl=true \
-        -Dopengl=es-egl -Dbuild-tests=false -Dbuildtype=release build/
+        -Dopengl=es-egl -Dbuild-tests=false -Dbuild-examples=false -Dbuildtype=release build/
       ninja -C build/ || mng_err
       ;;
     enlightenment)
