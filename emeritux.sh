69fb5906101e6c90e798f2961e027b1f3ecdcf86
Philippe Jean Guillaumie <similar@orange.fr>
2019-09-23 21:27:19 +0200

    Add comments

--- emeritux.sh
+++ emeritux.sh
@@ -720,8 +720,8 @@ remov_meson() {
 
 # Think twice before proceeding with the removal of these packages,
 # please review them carefully. If in doubt, take a screenshot
-# first for future reference, or better yet, keep the
-# currently installed set of development packages.
+# for later reference, or better yet, keep the currently
+# installed set of development packages.
 remov_bin_deps() {
   if [ ! -d $HOME/.local/lib/python3.6/site-packages/mesonbuild/ ]; then
     echo
